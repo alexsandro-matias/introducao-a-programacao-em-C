1578da61ccba9d6c961461629d2feb04674a06bc
AlexsandroMatias2017 <matiasalexsandro@gmail.com>
2019-11-06 12:32:40 -0300

    adicionada a questao 40 de vetores

---
+++ respostas/Lista 04 - vetores e matrizes/lista04Questao40/questao40.c
@@ -0,0 +1,5 @@
+ 
+//  Elabore um programa que faça leitura de vários números inteiros, até que se digite um
+// número negativo. O programa tem que retornar o maior e o menor número lido.
+
+
