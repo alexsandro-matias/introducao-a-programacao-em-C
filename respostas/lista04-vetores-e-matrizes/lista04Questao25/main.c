de485f9b25bc08d586d16af600f2915d16120921
alexsandro-matias <matiasalexsandro@gmail.com>
2020-08-14 05:06:10 -0300

    Pendente definição de vitória

--- respostas/lista04-vetores-e-matrizes/lista04Questao25/main.c
+++ respostas/lista04-vetores-e-matrizes/lista04Questao25/main.c
@@ -5,7 +5,7 @@
 
 /**
 
-25. Faça um programa para determinar a próxima jogada em um Jogo da Velha. Assumir que
+25. Faça um programa para determinar a próxima jogadaX em um Jogo da Velha. Assumir que
 o tabuleiro é representado por uma matriz de 3 x 3, onde cada posição representa uma
 das casas do tabuleiro. A matriz pode conter os seguintes valores -1, 0, 1 representando
 respectivamente uma casa contendo uma peça minha (-1), uma casa vazia do tabuleiro
@@ -14,49 +14,95 @@ Exemplo:
 
 **/
 
-int main()
+void carregamentoDosDados(int *game[TAMANHO][TAMANHO])
 {
-    int i = 0 , j = 0, jogo[TAMANHO][TAMANHO];
 
-
-    void limpaTela()
-    {
-        system("cls");
-    }
-
-    void aguardaMovimento()
-    {
-        getchar();
-    }
-
-    void jogada()
-    {
-        limpaTela();
-        aguardaMovimento();
-    }
-
-    for (i = 0 ; i < TAMANHO ; i++)
-    {
-        for (j = 0; j < TAMANHO ; j++ )
+        for (int i = 0 ; i < TAMANHO ; i++)
         {
-            jogo[i][j] = 0;
+            for (int j = 0; j < TAMANHO ; j++ )
+            {
+                game[i][j] = 0;
+            }
+
         }
-    }
 
-    jogada();
 
-    for (i = 0 ; i < TAMANHO ; i++)
+}
+
+void apresentacao(int *game[TAMANHO][TAMANHO])
+{
+    for (int i = 0 ; i < TAMANHO ; i++)
     {
-        for (j = 0; j < TAMANHO ; j++ )
+        for (int j = 0; j < TAMANHO ; j++ )
         {
-            printf("| %d |" , jogo[i][j]);
+            printf("  |%d|  " , game[i][j]);
         }
 
         printf("\n");
+
+    }
+
+}
+
+void limpaTela()
+{
+    system("cls");
+}
+
+
+void aguardaMovimento()
+{
+    getchar();
+}
+
+void jogadaX(int *game[TAMANHO][TAMANHO])
+{
+    int linha, coluna;
+    printf("Digite a linha e coluna da jogador -1: \n");
+    printf("Linha e Coluna: ");
+    scanf("%d %d", &linha, &coluna);
+
+    game[linha-1][coluna-1] = -1;
+
+    limpaTela();
+    apresentacao(game);
+    aguardaMovimento();
+}
+
+
+void jogadaY(int *game[TAMANHO][TAMANHO])
+{
+    int linha, coluna;
+    printf("Digite a linha e coluna da jogador 1: \n");
+    printf("Linha e Coluna: ");
+    scanf("%d %d", &linha, &coluna);
+
+    game[linha-1][coluna-1] = 1;
+
+    limpaTela();
+    apresentacao(game);
+    aguardaMovimento();
+}
+
+
+int main()
+{
+    int jogo[TAMANHO][TAMANHO], quantidadeX, quantidadeY = 0;
+
+    carregamentoDosDados(jogo);
+
+    apresentacao(jogo);
+
+     while(quantidadeX <= 3 || quantidadeY <= 3 )
+    {
+        jogadaX(jogo);
+        quantidadeX++;
+
+        jogadaY(jogo);
+        quantidadeY++;
     }
 
 
 
-    printf("Hello world!\n");
     return 0;
 }
